608f4bb83008ebf4f54e304f954cd258e454cd43
Ravindra Prasad <ravindraprasad@yahoo.com>
2023-06-16 19:17:41 +0530

    added requirements.txt and updated main.py

--- main.py
+++ main.py
@@ -3,4 +3,8 @@ from langchain.document_loaders import UnstructuredPDFLoader
 from langchain.indexes import VectorstoreIndexCreator
 from dotenv import load_dotenv
 
+load_dotenv()
+
 OPENAI_API_KEY = os.environ.get('OPENAI_API_KEY')
+
+print("OPENAI_API_KEY", OPENAI_API_KEY)
